714fbd8f345e3f6013ab1a6ba079242fdd403046
johnbane85 <junyu.siew@gmail.com>
2022-07-18 16:05:41 +0800

    commit

--- src/main/java/sg/edu/nus/iss/d11wkshp/D11wkshpApplication.java
+++ src/main/java/sg/edu/nus/iss/d11wkshp/D11wkshpApplication.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.iss.d11wkshp;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class D11wkshpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(D11wkshpApplication.class, args);
+		// SpringApplication.run(D11wkshpApplication.class, args);
+		SpringApplication app = new SpringApplication(D11wkshpApplication.class);
+
+		String port = "8000";
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+
+		if (cliOpts.containsOption("port")) {
+			port = cliOpts.getOptionValues("port").get(0);
+		}
+
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
+
 	}
 
 }
